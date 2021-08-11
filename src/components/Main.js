f122da4372bfe59dd56a90cd13364b082c22fa1c
XtremeGood <manglavishesh64@gmail.com>
2021-08-11 12:46:20 +0530

    fixed:sidebar search component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,57 +1,62 @@
 import { Grid } from "semantic-ui-react";
 import { useState, createContext } from "react";
-import { Tab } from "semantic-ui-react";
-import { Form } from "semantic-ui-react";
-import { useForm, FormProvider} from "react-hook-form";
-import { getProfileMenu, getTestMenu} from "./SideBar"
+import { Form, Tab, Input } from "semantic-ui-react";
+import { useForm, FormProvider } from "react-hook-form";
+import { getProfileMenu, getTestMenu } from "./SideBar"
 
 export const TestContext = createContext({});
 export const TabContext = createContext({});
+export const SearchContext = createContext({});
 
 const Main = () => {
   const [testList, setTestList] = useState({});
   const [counter, setCounter] = useState(0);
+  const [searchText, setSearchText] = useState("")
 
   const methods = useForm({ shouldUnregister: false });
 
   const [currentTabIndex, setCurrentTabIndex] = useState(1);
 
 
-  
-  const handleTestDeletion = (e, testID)=> {
+
+
+
+  const handleTestDeletion = (e, testID) => {
     const filterDict = (dict, filterFunc) =>
       Object.fromEntries(Object.entries(dict).filter(filterFunc));
-      
+
     setTestList((state) => filterDict(state, ([key, val]) => key !== testID));
-    
+
     methods.unregister(`patient.tests.${testID}`);
     setCurrentTabIndex(0);
   };
-  
-  const menuItems = [...getProfileMenu(), ...getTestMenu(testList, handleTestDeletion)];
+
+  const menuItems = [...getProfileMenu(), ...getTestMenu(testList, searchText, handleTestDeletion)];
 
 
   return (
     <Grid.Column width={16}>
-      <FormProvider {...{ ...methods}}>
+      <FormProvider {...{ ...methods }}>
         <TestContext.Provider
           value={{ counter, setCounter, testList, setTestList }}
         >
           <TabContext.Provider value={{ currentTabIndex, setCurrentTabIndex }}>
-            <Form
-              className="attached fluid segment"
-              method="post"
-              action={"http://localhost:8000"}
-            >
-              <Tab
-                menu={{ fluid: true, vertical: true, tabular: true }}
-                panes={menuItems}
-                activeIndex={currentTabIndex}
-                onTabChange={(e, { activeIndex }) => {
-                  setCurrentTabIndex(activeIndex);
-                }}
-              />
-            </Form>
+            <SearchContext.Provider value={{ searchText, setSearchText }}>
+              <Form
+                className="attached fluid segment"
+                method="post"
+                action={"http://localhost:8000"}
+              >
+                <Tab
+                  menu={{ fluid: true, vertical: true, tabular: true }}
+                  panes={menuItems}
+                  activeIndex={currentTabIndex}
+                  onTabChange={(e, { activeIndex }) => {
+                    setCurrentTabIndex(activeIndex);
+                  }}
+                />
+              </Form>
+            </SearchContext.Provider>
           </TabContext.Provider>
         </TestContext.Provider>
       </FormProvider>
--- src/components/SideBar/TestsMenu.js
+++ src/components/SideBar/TestsMenu.js
@@ -1,62 +1,76 @@
-import React from "react";
-import { Header, Button, Message, Tab } from "semantic-ui-react";
+import React, { useContext} from "react";
+import { Input, Header, Button, Message, Tab } from "semantic-ui-react";
 import TestDetails from "../FormComponents/TestDetails";
 import AddTestButton from "../FormComponents/AddTestButton";
-import Search from "./SearchItem";
+import {SearchContext} from "../Main";
 
-
+const SearchInput = () => {
+    const { searchText, setSearchText } = useContext(SearchContext);
+    return (
+        <Input
+            icon="search"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e, { value }) => {
+                setSearchText(value) }}
+        />
+    );
+};
 const getTestHeader = () => {
     return {
         menuItem: {
             header: true,
             content: (
-                <React.Fragment >
-                    <Header as="h3" >Tests</Header>{" "}
-                    <Search />
+                <React.Fragment>
+                    <Header as="h3">Tests</Header>
+                    <SearchInput />
                 </React.Fragment>
             ),
             active: false,
             disabled: true,
         },
         render: () => <Tab.Pane key={"Tests Heading"}></Tab.Pane>,
-    }
+    };
 };
 
-const getTestList = (testList, handleTestDeletion) => {
+const getTestList = (testList,searchText, handleTestDeletion) => {
+    const checkQueryinSearchText = (testID) => {
+        return (!searchText) || testList[testID].text.toLowerCase().includes(searchText.toLowerCase())
+    }
+    return Object.keys(testList)
+        .filter(checkQueryinSearchText)
+        .slice(0, 10)
+        .map((testID) => {
+            return {
+                menuItem: { content: testList[testID].text, color: "violet" },
 
-    return Object.keys(testList).map((testID) => {
-       
-        return {
-            menuItem: { content: testList[testID].text, color: "violet" },
+                render: () => (
+                    <Tab.Pane key={testID}>
+                        <Message
+                            attached
+                            header="Test Details"
+                            content="Fill out the test details."
+                        />
+                        <TestDetails testID={testID} />
+                        <AddTestButton />
+                        <Button
+                            color="red"
+                            type="button"
+                            onClick={(event) => handleTestDeletion(event, testID)}
+                        >
+                            Remove test
+                        </Button>
+                        <Button color="violet" type="submit">
+                            Submit
+                        </Button>
+                    </Tab.Pane>
+                ),
+            };
+        });
+};
 
-            render: () => (
-                <Tab.Pane key={testID}>
-                    <Message
-                        attached
-                        header="Test Details"
-                        content="Fill out the test details."
-                    />
-                    <TestDetails testID={testID} />
-                    <AddTestButton />
-                    <Button
-                        color="red"
-                        type="button"
-                        onClick={(event) => handleTestDeletion(event, testID)}
-                    >
-                        Remove test
-                    </Button>
-                    <Button color="violet" type="submit" >
-                        Submit
-                    </Button>
-                </Tab.Pane>
-            ),
-        };
-    })
-}
-
-
-const getTestMenu = (testList, handleTestDeletion) => {
-    return [getTestHeader(), ...getTestList(testList, handleTestDeletion)]
-}
+const getTestMenu = (testList,searchText, handleTestDeletion) => {
+    return [getTestHeader(), ...getTestList(testList,searchText, handleTestDeletion)];
+};
 
 export default getTestMenu;
